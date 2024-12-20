c082e0a3da2be4d541a2158dbeec7757ebd3cee1
MachA420 <85092945+MachA420@users.noreply.github.com>
2024-12-20 01:59:23 -0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -86,7 +86,7 @@ export default {
                         <p class="error" v-for="error of errors">{{ error }}</p>
                     </div>
                     <div class="og">
-                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
+                        <p class="type-label-md">GDPS made by FORM and <a href="" target="_blank">lucas</a></p>
                     </div>
                     <template v-if="editors">
                         <h3>List Editors</h3>
@@ -98,30 +98,21 @@ export default {
                             </li>
                         </ol>
                     </template>
-                    <h3>Submission Requirements</h3>
+                    <h3>In order to submit a record:s</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        1. You must have clicks in your recording.
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        2. You must have raw footage.
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        3. All FPS is allowed.
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        4. CBF is allowed.
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
-                    </p>
-                    <p>
-                        Do not use secret routes or bug routes
-                    </p>
-                    <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
-                    </p>
-                    <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        5. You must you the submitter to submit your record for review.
                     </p>
                 </div>
             </div>
